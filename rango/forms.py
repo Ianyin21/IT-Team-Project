3ccbf0ae0cef0c27f79ec0477e5906a8839518d3
2511643L <2511643L@student.gla.ac.uk>
2021-08-07 01:23:03 +0800

    create post

--- rango/forms.py
+++ rango/forms.py
@@ -18,8 +18,9 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the post.")
+    content = forms.CharField(max_length=900, help_text="Please enter the content of the post.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the post.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
